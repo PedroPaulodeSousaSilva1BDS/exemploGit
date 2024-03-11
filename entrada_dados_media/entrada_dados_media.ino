1d14ef54862ff76bbfd7c515d62bc8427c7d0768
Pedropaulosousilva <159209071+Pedropaulosousilva@users.noreply.github.com>
2024-03-11 08:02:18 -0300

    Add files via upload

---
+++ entrada_dados_media/entrada_dados_media.ino
@@ -0,0 +1,12 @@
+//Declaracão das variáveis / numéros Reais */
+float nota1, nota2, nota3, peso1, peso2, peso3, media;
+
+void setup() {
+  //Inicia a comunicação com o Serial monitor
+  Serial.begin(9600);
+
+  delay(1000);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+}
---
+++ entrada_dados_media_funcao/entrada_dados_media_funcao.ino
@@ -0,0 +1,70 @@
+/*Declaração das variáveis numéros Reais */
+float nota1, nota2, nota3, peso1, peso2, peso3, media;
+
+void setup() {
+  //Inicia a comunicação com o Serial monitor
+  Serial.begin(9600);
+
+  delay(1000);  //Espera estratégica
+}
+void loop() {
+  Serial.print("Digite a nota 1: ");
+
+  nota1 = aguardaEntradaNumerica();
+  Serial.println(nota1);
+
+  Serial.print("Digite a nota 2 :");
+
+  nota2 = aguardaEntradaNumerica();
+  Serial.println(nota2);
+
+  Serial.print("Digite a nota 3:");
+
+  nota3 = aguardaEntradaNumerica();
+  Serial.println(nota3);
+  //Atribuição dos pesos
+  Serial.print("Digite o peso 1 :");
+
+peso1:
+  aguardaEntradaNumerica();
+  Serial.println(peso1);
+
+  //Atribuição dos pesos
+peso2:
+  aguardaEntradaNumerica();
+  Serial.println(peso2);
+
+  //Atribuição dos pesos
+  Serial.print("Digite o peso 3 :");
+
+peso3:
+  aguardaEntradaNumerica();
+  Serial.println(peso3);
+
+  //Cálculo da média ponderada:
+  media = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3);
+
+  //Exibe o valor da média calculada
+  Serial.print("Media ponderada : ");
+  Serial.println(media, 1);
+  delay(1000);
+
+}
+
+//criando uma função chamada
+float aguardaEntradaNumerica() {
+  while (!Serial.available() > 0) {
+    //Aguarda até que um valor seja digitado
+  }
+  //   Verifica se a entrada é numérica ou ponto
+  while (!isdigit(Serial.peek()) && Serial.peek() != '.') {
+    //Descarta a entrada não numérica
+    Serial.read();
+    while (Serial.available() == 0) {
+      //Aguarda a nova entrada do usuário
+    }
+  }
+
+  //retornar o valor digitado no serial em tipo float
+  return Serial.parseFloat();
+}
